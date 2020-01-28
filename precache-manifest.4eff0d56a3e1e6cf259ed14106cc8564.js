ed46812b08d685ba7730214fa830ae6d386a7033
Jampa Uchoa <joaopcu@gmail.com>
2020-01-28 16:49:08 -0300

    Updates

---
+++ precache-manifest.4eff0d56a3e1e6cf259ed14106cc8564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3f0e0f37546448487c1714d05a6818",
+    "url": "/AutoEssay-frontend/index.html"
+  },
+  {
+    "revision": "bb9657e38c6881c2f1bd",
+    "url": "/AutoEssay-frontend/static/css/main.b8027c58.chunk.css"
+  },
+  {
+    "revision": "b1289d38cdc2b23b0c3b",
+    "url": "/AutoEssay-frontend/static/js/2.52630ddc.chunk.js"
+  },
+  {
+    "revision": "4dc5f148c1744a33c0f8d3c87e43c131",
+    "url": "/AutoEssay-frontend/static/js/2.52630ddc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb9657e38c6881c2f1bd",
+    "url": "/AutoEssay-frontend/static/js/main.a29a8ec6.chunk.js"
+  },
+  {
+    "revision": "34b5bd5a3b32b3c8484c",
+    "url": "/AutoEssay-frontend/static/js/runtime-main.72bef861.js"
+  }
+]);
