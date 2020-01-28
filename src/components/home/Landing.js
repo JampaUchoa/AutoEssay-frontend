5780d324021115afbdd57bf288806139f70769a2
Jampa Uchoa <joaopedrouchoa@hotmail.com>
2020-01-27 23:47:30 -0300

    preview mode

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,29 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './landing.scss';
 
 export default function Landing() {
 
-  return (
-      <div className="essay-container">
-        <div className="form">
-          <div className="title" >
-            <input type="text" placeholder="Escreva aqui seu titulo"/>
-          </div>
-          <div className="essay">
-            <textarea rows="25"/>
-          </div>
-        </div>
-        
-        <div className="actions">
-          <button>
-            Revisar
-          </button>
-          <button>
-            Avaliar
-          </button>
-        </div>
+  const emptyForm = {
+    title: "",
+    essay: "",
+  };
 
+  const [form, setForm] = useState(emptyForm)
+
+  // Can be "writing", "revising", "evaluated"
+  const [status, setStatus] = useState("writing")
+
+  const changeForm = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const postEssay = (e) => {
+    /*
+    //TODO to a POST to django with form content
+    {
+      title: STRING,
+      essay: STRING
+    }
+    */
+    setStatus("evaluated");
+  }
+
+
+  return (
+    <div className="essay-container">
+      <div className={`form ${status}`}>
+        <div className="title" >
+          <input type="text" name="title" required onChange={changeForm} value={form.title} placeholder="Escreva aqui seu titulo" />
+        </div>
+        <div className="essay">
+          <textarea name="essay" required onChange={changeForm} value={form.essay} rows="25" />
+        </div>
       </div>
+
+      <Actions status={status} postEssay={postEssay} setStatus={setStatus} />
+    </div>
   );
+}
+
+function Actions({ status, postEssay, setStatus }) {
+
+  let content = "";
+
+  if (status === "writing") {
+    content = (
+      <>
+        <button onClick={() => setStatus("revising")}>
+          Revisar
+        </button>
+        <button onClick={postEssay}>
+          Avaliar
+        </button>
+      </>
+    )
+  } else if (status === "revising") {
+    content = (
+      <>
+        <button onClick={() => setStatus("writing")}>
+          Editar
+        </button>
+        <button onClick={postEssay}>
+          Avaliar
+        </button>
+      </>
+    )
+  } else {
+    content = (
+      <>
+        <button>
+          Nova redação
+        </button>
+      </>
+    )
+  }
+
+  return (
+    <div className="actions">
+      {content}
+    </div>
+  )
+
+
 }
 
