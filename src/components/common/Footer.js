2f1cac9257824a53fa5694e64c8d66faf6803bd9
Jampa Uchoa <joaopcu@gmail.com>
2020-01-28 16:35:15 -0300

    improve buttons

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -8,22 +8,22 @@ export default function Footer() {
         <footer>
             <div className="container">
                 <div className="social-icons">
-                    <a href="https://www.instagram.com/livoxapp/" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
+                    <a href="#" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
                         <FiInstagram/>
                     </a>
-                    <a href="https://www.facebook.com/livoxtablet/" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
+                    <a href="#" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
                         <FiFacebook/>
                     </a>
 
-                    <a href="https://twitter.com/livoxtablet/" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
+                    <a href="#" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
                         <FiTwitter/>
                     </a>
 
-                    <a href="https://www.youtube.com/channel/UCLjj9TZsQ-RVA3xe3_Moi9A/" aria-label="Youtube" target="_blank" rel="noopener noreferrer">
+                    <a href="#" aria-label="Youtube" target="_blank" rel="noopener noreferrer">
                         <FiYoutube/>
                     </a>
                 
-                    <a href="https://api.whatsapp.com/send?phone=5531984596650&text=Ola!%20gostaria%20de%20tirar%20uma%20duvida" aria-label="Whatsapp" target="_blank" rel="noopener noreferrer">
+                    <a href="#" aria-label="Whatsapp" target="_blank" rel="noopener noreferrer">
                         <FiPhone/>
                     </a>    
                 </div>
